a131ef43a915c43d02c9949406c588396aaafe1d
StefanoRe <re.stefano@gmail.com>
2025-11-01 18:32:49 +0100

    add more examples

--- 06_loops_and_controls.c
+++ 06_loops_and_controls.c
@@ -30,9 +30,62 @@ int block_block(void) {
 	return 0;
 }
 
+int block_goto(void) {
+	printf("** sezione GOTO **\n");
+	int i = 0;
+
+again:
+	printf("%d\n",i);
+	i++;
+	if (i < 10) goto again;
+	return 0;
+}
+
+int block_while(void) {
+	printf("** sezione WHILE **\n");
+	int i = 0;
+	while (i < 10) {
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+}
+
+int block_goto_2(void) {
+	printf("** sezione GOTO loop 2\n");
+	int i = 0;
+loop:
+	if (!(i < 10)) goto next;
+	printf("%d\n",i);
+	i++;
+	goto loop;
+next:
+	return 0;
+}
+
+void conta(int start, int end) {
+	if (start > end) return;
+	printf("%d\n", start);
+	conta(start + 1, end); 
+}
+
+int block_recursion(void) {
+	printf("** sezione RECURSION **\n");
+	conta(0,9);
+	return 0;
+}
+
 int main(void) {
 	if_block();
 	space();
 	block_block();
+	space();
+	block_goto();
+	space();
+	block_while();
+	space();
+	block_goto_2();
+	space();
+	block_recursion();
 	return 0;
 }
